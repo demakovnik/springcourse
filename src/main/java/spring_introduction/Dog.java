bd300b0d363f91dd8f7b2c9811249f26852c0b89
Nikita Demakov <demakoffnik@gmail.com>
2020-11-29 17:15:30 +0700

    Add lesson 7

---
+++ src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,13 @@
+package spring_introduction;
+
+public class Dog implements Pet{
+
+    public Dog() {
+        System.out.println("Dog is created");
+    }
+
+    @Override
+    public void say() {
+        System.out.println("Bow-Wow");
+    }
+}
