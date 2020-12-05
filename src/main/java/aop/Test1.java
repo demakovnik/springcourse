c64cfce263462055d191cbeeb91efb0c3ff21c50
demakovnik <demakoffnik@gmail.com>
2020-12-05 14:43:10 +0700

    Lesson 27. AOP. Pointcut declaration

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -8,9 +8,9 @@ public class Test1 {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        Book book = context.getBean("book",Book.class);
-        uniLibrary.getBook(book);
-        //uniLibrary.getMagazine();
+        //Book book = context.getBean("book",Book.class);
+        uniLibrary.getBook();
+        uniLibrary.getMagazine();
         //uniLibrary.returnBook();
         /*SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
         schoolLibrary.getBook();*/
--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class UniLibrary extends AbstractLibrary{
 
-    public void getBook(Book book) {
-        System.out.println("Мы берем книгу " + book.getName() + " в университетской библиотеке");
+    public void getBook() {
+        System.out.println("Мы берем книгу в университетской библиотеке");
     }
 
     public String returnBook() {
