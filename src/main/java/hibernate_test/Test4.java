0c0d9ac26a0cd6461408fbcf11dc030a9e54c7fe
demakovnik <demakoffnik@gmail.com>
2020-12-13 12:15:03 +0700

    Lesson 44. Deleting Java Objects from DB. Part 1

---
+++ src/main/java/hibernate_test/Test4.java
@@ -0,0 +1,28 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+
+        try(SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()) {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class,1);
+            session.delete(employee);
+
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        }
+
+
+    }
+}
